41d4f644961b1074570a76ddc013a6370b90cdec
BasirArsy <tugaskampus540@gmail.com>
2025-10-23 14:03:46 +0700

    Use Destructuring Assignment to Assign Variables from Arrays

---
+++ ES6/soal12.js
@@ -0,0 +1,14 @@
+/* Use Destructuring Assignment to Assign Variables from Arrays */
+
+// const [a, b] = [1,2,3];
+// console.log(a,b);
+// const x = "Basir";
+// const y = "arsy";
+// console.log(x,y);
+// console.log(x);
+// console.log(y);
+
+let a = 8;
+let b = 6;
+[a, b] = [b, a]
+console.log(a);
