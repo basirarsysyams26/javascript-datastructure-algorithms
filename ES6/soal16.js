fb3a6def0c4e71ac50356b4eecd85139a724aad6
BasirArsy <tugaskampus540@gmail.com>
2025-10-24 10:54:32 +0700

    Write Concise Object Literal Declarations Using Object Property Shorthand

---
+++ ES6/soal16.js
@@ -0,0 +1,13 @@
+// const createPerson = (firstName, age, gender) => {
+//   // Only change code below this line
+//   return { nama: firstName, age, gender };
+// };
+const createPerson = (name, age, gender) => ({name, age, gender});
+console.log(createPerson("Basir", "21", "Male"));
+
+const person = {
+    firstName : "basir",
+    age: 21
+};
+const {nama: firstName, age} = {person};
+console.log(nama);
