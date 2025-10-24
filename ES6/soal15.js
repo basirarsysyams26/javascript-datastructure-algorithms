65257ffc57f4d10fca0ef3dd16803e622d937f71
BasirArsy <tugaskampus540@gmail.com>
2025-10-24 10:23:57 +0700

    Create Strings using Template Literals

---
+++ ES6/soal15.js
@@ -0,0 +1,15 @@
+/* Create Strings using Template Literals */
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"],
+};
+function makeList(arr) {
+  const failureItems = arr.map(item => `<li class="text-warning">${item}</li>`);
+  // for (let item of arr) {
+  //   failureItems.push(`<li class="text-warning">${item}</li>`)
+  // }
+  return failureItems;
+}
+const failuresList = makeList(result["failure"]);
+console.log(failuresList);
