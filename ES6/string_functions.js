cb4b09c5a3b5a3014865bf61baf97e32b9a64d6d
BasirArsy <tugaskampus540@gmail.com>
2025-10-26 20:23:37 +0700

    use export to share a code black

---
+++ ES6/string_functions.js
@@ -0,0 +1,15 @@
+/* Use export to Share a Code Block */
+const add = (x, y) => {
+  return x + y;
+};
+export { add };
+
+const uppercaseString = (string) => {
+  return string.toUpperCase();
+};
+const lowercaseString = (string) => {
+  return string.toLowerCase();
+};
+export { uppercaseString, lowercaseString, x };
+
+let x = "gear";
